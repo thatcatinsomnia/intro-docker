7959c21e4e50dd4edb62934713ec41d23bb4bb29
Ruei <ruei@dev.null>
2024-05-10 15:54:12 +0800

    :sparkles: update config to make windows work

--- 08-bind-mounts/vite.config.ts
+++ 08-bind-mounts/vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    watch: {
+      // if you are using windows os, add this line to make hot reload work 
+      usePolling: true
+    }
   }
 })
